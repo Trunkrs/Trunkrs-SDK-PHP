2296fb9cad7ca9c5fe295edd100f0b61b9e5599c
Leo <leonard@trunkrs.nl>
2020-03-10 11:44:15 +0100

    Add: Shipment details

--- tests/ShipmentDetailsV1MappingTest.php
+++ tests/ShipmentDetailsV1MappingTest.php
@@ -4,6 +4,10 @@ namespace Trunkrs\SDK;
 
 class ShipmentDetailsV1MappingTest extends APIV1TestCase {
     public function testShipmentDetailsV1Mapping() {
-        $this->assertEquals(true, true);
+        $srcDetails = Mocks::getFakeDetails();
+
+        $json = $srcDetails->serialize();
+
+        self::assertEquals($srcDetails->reference, $json['orderReference']);
     }
 }
