3d2a48b345c43ad7cabeee745be77a4db9de7055
Leonard Breitkopf <leonard@trunkrs.nl>
2021-02-22 13:52:44 +0100

    Bump: v2.1.4

--- lib/Settings.php
+++ lib/Settings.php
@@ -38,7 +38,7 @@ class Settings {
     /**
      * @var string The current version of the SDK.
      */
-    public static $sdkVersion = '2.1.3';
+    public static $sdkVersion = '2.1.4';
 
     /**
      * Sets the client credentials that will be used in subsequent requests.
